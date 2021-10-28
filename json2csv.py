2f570949088d91abe39b88514b52af62e55d4e07
Jerry Eshbaugh <j2m2e2@gmail.com>
2021-10-28 03:43:25 -0600

    Drill into datasets to display correct information for each row in this nested json input file

--- json2csv.py
+++ json2csv.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+import json
 
 in_file = './data/USAspending-data-catalog.json'
 out_file = './data/USAspending-data-catalog.csv'
 
 print( 'Coverting ', in_file, ' to ', out_file )
 
-df = pd.read_json (in_file)
-df.to_csv (out_file, index = None)
+# Read the input file into a Data Frame
+
+
+with open(in_file) as data_file:
+    data = json.load(data_file)
+
+df = pd.json_normalize( data, 'dataset')
+#df = pd.read_json (in_file)
+
+print( df )
+
+df.to_csv (out_file, index = True, header = True)
